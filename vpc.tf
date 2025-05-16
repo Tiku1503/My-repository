5c68be40ac0c58f96877d8401b71665ca5757e7a
Tiku1503 <tikuenyang23@gmail.com>
2025-05-15 22:45:54 -0500

    attributes correction

--- vpc.tf
+++ vpc.tf
@@ -7,8 +7,8 @@ terraform {
     bucket     = "githubbucket91"
     key        = "terraform/terraform.tfstate"
     region     = "us-east-1"
-    access_key = var.access_key
-    secret_key = var.aws_secret_key
+    aws_access_key = var.aws_access_key
+    aws_secret_key = var.aws_secret_key
   }
 }
 
