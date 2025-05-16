0cafbaa9405e77f4d027fc3c2df227a40593a2cb
Tiku1503 <tikuenyang23@gmail.com>
2025-05-15 22:32:23 -0500

    added from fresh

---
+++ vpc.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "githubbucket91"
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+variable "aws_access_key" {
+  type        = string
+}
+variable "aws_secret_key" {
+  type        = string
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
