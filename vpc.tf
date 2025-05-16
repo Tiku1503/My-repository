baaea71cf0564d2af3b5894ce308b450d2f36f9c
Tiku1503 <tikuenyang23@gmail.com>
2025-05-15 22:44:45 -0500

    Key declaration

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ terraform {
     bucket     = "githubbucket91"
     key        = "terraform/terraform.tfstate"
     region     = "us-east-1"
-    access_key = var.aws_access_key
+    access_key = var.access_key
     secret_key = var.aws_secret_key
   }
 }
